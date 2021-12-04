3b79d96aa08b70a39afcba84b14d5a0f1ee6511e
Mario Carmona Segovia <mcs2000carmona@correo.ugr.es>
2021-12-05 00:02:32 +0100

    Reconocimiento en el menú

--- NPIapp/app/src/main/java/com/example/npiapp/MainActivity.java
+++ NPIapp/app/src/main/java/com/example/npiapp/MainActivity.java
@@ -19,6 +19,7 @@ import android.os.Bundle;
 import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
+import android.speech.tts.TextToSpeech;
 import android.util.Log;
 import android.view.View;
 import android.content.Intent;
@@ -32,13 +33,14 @@ import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class MainActivity extends AppCompatActivity  implements SensorEventListener {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     public static float saldo = 20.0f;
     public static String TUI = "0001246933";
     public static final Integer RecordAudioRequestCode = 1;
     private SpeechRecognizer speechRecognizer;
     final Intent speechRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+    private TextToSpeech speaker;
     // Manejador de sensores
     private SensorManager sensorManager;
     // Sensor de proximidad
@@ -51,7 +53,6 @@ public class MainActivity extends AppCompatActivity  implements SensorEventListe
     protected void onCreate(Bundle savedInstanceState) {
         inicio = 0;
         fin = 0;
-        Log.i("INFO_SPEECH","ON create");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         if(ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED){
@@ -82,7 +83,6 @@ public class MainActivity extends AppCompatActivity  implements SensorEventListe
             @Override
             public void onRmsChanged(float v) {
                 fin = System.nanoTime();
-                Log.i("INFO_SPEECH","El sonido es "+v);
                 if(v < 0 && (fin - inicio)/1000000000 > 3)
                     speechRecognizer.stopListening();
             }
@@ -105,25 +105,18 @@ public class MainActivity extends AppCompatActivity  implements SensorEventListe
             @Override
             public void onResults(Bundle bundle) {
 
-                //micButton.setImageResource(R.drawable.ic_mic_black_off);
                 ArrayList<String> data = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
                 String datos = data.get(0);
-                Log.i("INFO_SPEECH",data.get(0));
-                Pattern pattern = Pattern.compile("(.)*(abr(.)*|entr(.)*)(.)*guía(.)*", Pattern.CASE_INSENSITIVE);
-                Matcher matcher = pattern.matcher(datos);
-                boolean matchFound = matcher.find();
-                if(matchFound){
-                    Intent intent = new Intent(MainActivity.this, Guidance.class);
-                    startActivity(intent);
+
+                boolean reconocido = false;
+
+                reconocido = reconocido || reconocerIrGuidance(datos);
+
+                reconocido = reconocido || reconocerIrCanteenMenu(datos);
+
+                if(!reconocido) {
+                    speaker.speak("Reconocimiento fallido. Vuelve a intentarlo", TextToSpeech.QUEUE_FLUSH, null);
                 }
-                pattern = Pattern.compile("(.)*(abr(.)*|entr(.)*)(.)*menú(.)*", Pattern.CASE_INSENSITIVE);
-                matcher = pattern.matcher(datos);
-                matchFound = matcher.find();
-                if(matchFound){
-                    Intent intent = new Intent(MainActivity.this, CanteenMenu.class);
-                    startActivity(intent);
-                }
-                //editText.setText(data.get(0));
             }
 
             @Override
@@ -137,8 +130,37 @@ public class MainActivity extends AppCompatActivity  implements SensorEventListe
             }
         });
 
-        //speechRecognizer.startListening(speechRecognizerIntent);
+        speaker = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int i) {
 
+            }
+        });
+        speaker.setLanguage(Locale.getDefault());
+    }
+
+    boolean reconocerIrGuidance(String datos) {
+        Pattern pattern = Pattern.compile("(.)*(abr(.)*|entr(.)*)(.)*guía(.)*", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(datos);
+        boolean reconocido = matcher.find();
+        if(reconocido){
+            Intent intent = new Intent(MainActivity.this, Guidance.class);
+            startActivity(intent);
+        }
+
+        return reconocido;
+    }
+
+    boolean reconocerIrCanteenMenu(String datos) {
+        Pattern pattern = Pattern.compile("(.)*(abr(.)*|entr(.)*)(.)*menú(.)*", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(datos);
+        boolean reconocido = matcher.find();
+        if(reconocido){
+            Intent intent = new Intent(MainActivity.this, CanteenMenu.class);
+            startActivity(intent);
+        }
+
+        return reconocido;
     }
 
     @Override
@@ -169,62 +191,15 @@ public class MainActivity extends AppCompatActivity  implements SensorEventListe
         // Gestión del sensor de proximidad
         if (sensorEvent.sensor.getType() == Sensor.TYPE_PROXIMITY) {
             float distancia = sensorEvent.values[0];
-            Log.i("INFO_SPEECH","la distancia es " + distancia);
             if (distancia < 1) {
                 speechRecognizer.startListening(speechRecognizerIntent);
                 inicio = System.nanoTime();
                 Toast.makeText(MainActivity.this,
                         "Escuchando...", Toast.LENGTH_LONG)
                         .show();
-                Log.i("INFO_SPEECH","tamos escuchando");
-                //startActivityForResult(speechRecognizerIntent,10);
-                Log.i("INFO_SPEECH","Proximidad activada");
             }
         }
 
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (resultCode == RESULT_OK && data != null) {
-            switch (requestCode) {
-                case 10:
-                    String datos = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS).get(0);
-                    Log.i("INFO_SPEECH",datos);
-                    Toast.makeText(this,
-                            datos, Toast.LENGTH_LONG)
-                            .show();
-
-                    Pattern pattern = Pattern.compile("(.)*(abr(.)*|entr(.)*)(.)*guía(.)*", Pattern.CASE_INSENSITIVE);
-                    Matcher matcher = pattern.matcher(datos);
-                    boolean matchFound = matcher.find();
-                    if(matchFound){
-                        Intent intent = new Intent(this, Guidance.class);
-                        startActivity(intent);
-                    }
-                    pattern = Pattern.compile("(.)*(abr(.)*|entr(.)*)(.)*menú(.)*", Pattern.CASE_INSENSITIVE);
-                    matcher = pattern.matcher(datos);
-                    matchFound = matcher.find();
-                    if(matchFound){
-                        Intent intent = new Intent(this, CanteenMenu.class);
-                        startActivity(intent);
-                    }
-
-                    /*int intFound = getNumberFromResult(data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS));
-                    if (intFound != -1) {
-                        FIRST_NUMBER = intFound;
-                        firstNumTextView.setText(intFound);
-                    } else {
-                        Toast.makeText(getApplicationContext(), "Sorry, I didn't catch that! Please try again", Toast.LENGTH_LONG).show();
-                    }*/
-                    break;
-
-            }
-        } else {
-            Toast.makeText(getApplicationContext(), "Failed to recognize speech!", Toast.LENGTH_LONG).show();
-        }
     }
 
     @Override
@@ -237,17 +212,6 @@ public class MainActivity extends AppCompatActivity  implements SensorEventListe
             ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.RECORD_AUDIO},RecordAudioRequestCode);
         }
     }
-
-    /*@Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions,
-                grantResults);
-        if (requestCode == RecordAudioRequestCode &&
-                grantResults.length > 0 ){
-            if(grantResults[0] == PackageManager.PERMISSION_GRANTED)
-                Toast.makeText(this,"Permission Granted",Toast.LENGTH_SHORT).show();
-        }
-    }*/
 
 
     public void launchGuidance(View view) {
